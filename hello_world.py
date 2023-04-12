1c336c3e0f4300401376bcbc10fcae7c759927af
macaitisA <macaitis.a@gmail.com>
2023-04-12 16:12:07 +0300

    labas

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,13 @@
 
 
 
-vardas = input("Įveskite savo vardą: ")
-metai = input("Įveskite savo amžių: ")
-metai_iki_100 = 100 - int(metai)
-rezultatas = 2023 + metai_iki_100
-print(f"Sveiki, {vardas}! Jūs sukaksite 100 metų {rezultatas}-aisiais.")
+#vardas = input("Andrius: ")
+#metai = input("34: ")
+#metai_iki_100 = 100 - int(metai)
+#rezultatas = 2023 + metai_iki_100
+#print(f"Sveiki, {vardas}! Jūs sukaksite 100 metų {rezultatas}-aisiais.")
 
-ugis_cm = input("Įveskite savo ūgį centimetrais: ")
+ugis_cm = input("iveskite savo ugi: ")
 ugis_m = int(ugis_cm) / 100
 print(f"Jūsų ūgis yra {ugis_cm} cm arba {ugis_m:.2f} m.")
 
