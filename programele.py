00fc0054e4b1219be0e72cb310a3c7e80d1f2cd9
macaitisA <macaitis.a@gmail.com>
2023-04-13 14:47:34 +0300

    sarasai

---
+++ programele.py
@@ -0,0 +1,104 @@
+x = 5
+x = 10
+print(x)
+
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+x = 5
+print(type(x))
+
+mano_kintamasis = 10
+print(mano_kintamasis)
+
+x = 4
+X = 9
+print(x)
+print(X)
+
+x = 5
+print(x)
+x = "hello, world"
+print(x)
+
+x = 5
+y = x
+print(y)
+x = 10
+print(x)
+print(y)
+a = 10
+b = 3.14
+c = 2 + 3j
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+z = 2 * 3
+print(z)
+
+b = 10 // 3
+print(b)
+
+x=5
+X=5
+atsakymas1 = x + X
+print(atsakymas1)
+
+a = 10
+b = 5
+if a == b:
+    print("a ir b yra lygus")
+else:
+    print("a ir b nera lygus")
+
+x = True
+y = False
+print(x)
+print(y)
+
+#amzius = int(input("iveskite savo amziu"))
+#if amzius >= 18:
+#   print("jus esate pilnametis")
+#else:
+#    print("jus nesate pilnametis")
+
+#skaicius = int(input("iveskite skaiciu"))
+#if skaicius >= 5 and skaicius <= 10:
+#    print("skaiicus yra tarp 5 ir 10")
+#else:
+#    print("skaicius nera tarp 5 ir 10")
+
+#a = int(input("iveskite pirma skaiciu"))
+#b = int(input("iveskite antar skaiciu"))
+#if a > 10 and b > 10:
+#    print("skaicius didenis uz 10")
+#else:
+# print("abu skaiciai maziau 10")
+
+#ugis_cm = input("iveskite savo ugi: ")
+#ugis_m = int(ugis_cm) / 100
+#print(f"Jūsų ūgis yra {ugis_cm} cm arba {ugis_m:.2f} m.")
+
+#raides = "mobis dikas"
+#print(raides[0])
+#print(raides[10])
+#print(raides[0:5])
+#print(raides[5:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
